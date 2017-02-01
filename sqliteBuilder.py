3139627fecac5838ab1af9bb5353b7537ba5f31c
John Keck <robertjkeck2@gmail.com>
2017-01-31 19:28:46 -0500

    add add_row method, change some code semantics

--- sqliteBuilder.py
+++ sqliteBuilder.py
@@ -25,8 +25,8 @@ class SqliteBuilder():
 			formatted_columns.append(column)
 
 		column_string = ', '.join(formatted_columns)
-		sql_function = 'CREATE TABLE'
-		sql_query = '{} {}({})'.format(sql_function,
+		create_function = 'CREATE TABLE'
+		create_query = '{} {}({})'.format(create_function,
 			                       tablename,
 			                       column_string)
 
@@ -34,7 +34,7 @@ class SqliteBuilder():
 		conn = sqlite3.connect(self.db)
 		c = conn.cursor()
 		try:
-			c.execute(sql_query)
+			c.execute(create_query)
 		except:
 			print("Table already exists.")
 		conn.commit()
@@ -46,21 +46,54 @@ class SqliteBuilder():
 		formatted_column = "{} ADD COLUMN {} {}".format(tablename,
 			                                        column_setup[0][0],
 			                                        column_setup[0][1])
-		sql_function = 'ALTER TABLE'
-		sql_query = '{} {}'.format(sql_function, formatted_column)
+		alter_function = 'ALTER TABLE'
+		alter_query = '{} {}'.format(alter_function, formatted_column)
 		
 		#Add column to database
 		conn = sqlite3.connect(self.db)
 		c = conn.cursor()
 		try:
-			c.execute(sql_query)
+			c.execute(alter_query)
 		except:
 			print("Error adding new column.")
 		conn.commit()
 		conn.close()
 
-	def insert_row(self):
-		pass
+	def add_row(self, tablename, row_setup):
+		
+		#Determine current table row and first column name
+		conn = sqlite3.connect(self.db)
+		c = conn.cursor()
+		c.execute('SELECT COUNT(*) FROM {}'.format(tablename))
+		count = c.fetchall()
+		c.execute('PRAGMA TABLE_INFO({})'.format(tablename))
+		col_names = [i[1] for i in c.fetchall()]
+		first_col = col_names[0]
+		conn.close()
+
+		#New row setup
+		columns = [i[0] for i in row_setup]
+		columns.insert(0, str(first_col))
+		values = [str(i[1]) for i in row_setup]
+		values.insert(0, str(count[0][0] + 1))
+		formatted_columns = ', '.join(columns)
+		formatted_values = ', '.join(values)
+		insert_function = 'INSERT INTO'
+		insert_query = '{} {} ({}) VALUES ({})'.format(insert_function,
+			                                       tablename,
+			                                       formatted_columns,
+			                                       formatted_values)
+	
+		#Insert row to database
+		conn = sqlite3.connect(self.db)
+		c = conn.cursor()
+		try:
+			c.execute(insert_query)
+		except:
+			print("Error adding new row.")
+		conn.commit()
+		conn.close()
+
 	def update(self):
 		pass
 
@@ -68,6 +101,10 @@ a = SqliteBuilder('test_db.sqlite')
 d = 'test'
 e = True
 c = [("a", "TEXT"),("b", "INTEGER"),("c", "INTEGER")]
-f = [("e", "TEXT")]
-#a.create(d,c,e)
+f = [("d", "TEXT")]
+g = [("b", 2),("c", 3),("d", 4)]
+h = [("b", 3),("c", 4),("d", 5)]
+a.create(d,c,e)
 a.add_column(d,f)
+a.add_row(d,g)
+a.add_row(d,h)
